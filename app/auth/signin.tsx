fede6d70c1e820379338a6058f00de7f19abe105
rblood79 <rblood79@gmail.com>
2025-03-14 13:59:30 +0900

    supabase.client 경로 변경 및 환경 설정 파일 추가, CSS 스타일 및 레이아웃 개선

--- app/auth/signin.tsx
+++ app/auth/signin.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "@remix-run/react"; // 추가: useNavigate import
-import { supabase } from "../supabase.client";
+import { supabase } from "../env/supabase.client";
 
 export default function Signin() {
   const [email, setEmail] = useState("");
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import {
+/*import {
   Links,
   Meta,
   Outlet,
@@ -6,7 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { useEffect, useState } from "react";
-import { supabase, Session } from './supabase.client';
+import { supabase, Session} from './env/supabase.client';
 
 export default function Root() {
   const [session, setSession] = useState<Session | null>(null);
@@ -28,16 +28,78 @@ export default function Root() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" /> {/* 인코딩 지정 추가 */}
+        <meta charSet="utf-8" />
         <Links />
         <Meta />
       </head>
       <body>
         <header>notice</header>
-        <Outlet context={{ session }} /> {/* 세션을 context로 전달 */}
+        <Outlet context={{ session }} />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}*/
+
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { useEffect, useState } from "react";
+import { supabase, Session } from './env/supabase.client';
+
+import "./tailwind.css";
+import "./root.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    as: "style",
+    href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css",
+    crossOrigin: "anonymous",
+  }
+];
+
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+  return <Outlet context={{ session }} />;
 }
