c7e2d0f44dff1b1cfd0f233b19b178b76d351259
rblood79 <47205798+rblood79@users.noreply.github.com>
2025-03-19 01:22:34 +0900

    vite.config.ts 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/supabase": {
+        target: "https://msuyfmthxwnjqsknxbap.supabase.co",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/supabase/, ""),
+      },
+    },
+  }
 })
