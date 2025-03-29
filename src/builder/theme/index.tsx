8c0e94b44ac1c1aab8d7771e390ea3bf33b28b05
rblood79 <rblood79@gmail.com>
2025-03-30 01:51:27 +0900

    Refactor: Update icon properties in Pages component and enhance Theme component with detailed color descriptions and usage guidelines

--- src/builder/theme/index.tsx
+++ src/builder/theme/index.tsx
@@ -7,7 +7,32 @@ function Theme() {
             <div className="theme-container">
                 <div className="theme-item">
                     <div className="theme-item-title">
-                        <h4>Headline</h4>
+                        <h4>Basic</h4>
+                        <p>Core colors</p>
+                        <span>Override or set key colors that will be used to generate a color palettes and schemes</span>
+                        <ul>
+                            <li>
+                                <span>Primary</span>
+                                <span>Act as custom source color</span>
+                            </li>
+                            <li>
+                                <span>Secondary</span>
+                            </li>
+                            <li>
+                                <span>Tertiary</span>
+                            </li>
+                            <li>
+                                <span>Error</span>
+                            </li>
+                            <li>
+                                <span>Neutral</span>
+                                <span>Used for background and surfaces</span>
+                            </li>
+                            <li>
+                                <span>Neutral Variant</span>
+                                <span>Used for medium emphasis andvariants</span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
