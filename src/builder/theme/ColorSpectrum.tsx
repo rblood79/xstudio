2044fbf5a53e244adb2698be87a8d3019ec5bac2
rblood79 <rblood79@gmail.com>
2025-04-07 18:01:52 +0900

    Refactor ColorSpectrum component to simplify button and toggle button styles using Tailwind CSS, enhancing consistency and usability. Update CSS for improved button states and group layout.

--- src/builder/theme/ColorSpectrum.tsx
+++ src/builder/theme/ColorSpectrum.tsx
@@ -642,28 +642,21 @@ export const ColorSpectrum: React.FC<ColorSpectrumProps> = ({
                     <div className="category-container">
                         <h4 className="category-title">Buttons</h4>
                         <div className="category-content">
-                            <Button
-                                className={`px-4 py-2 rounded-md text-white bg-primary-500 hover:bg-primary-600 active:bg-primary-700`}
-                            >Primary Button
-                            </Button>
-                            <Button
-                                className={`px-4 py-2 rounded-md border-2 text-primary-500 bg-primary-50 border-primary-600`}
-                            >
-                                Secondary Button
-                            </Button>
+                            <Button className={`button`}>Primary Button</Button>
+                            <Button className={`button-line`}>Secondary Button</Button>
 
-                            <ToggleButton className="toggle-button">
+                            <ToggleButton className="toggle">
                                 ToggleButton
                             </ToggleButton>
 
-                            <ToggleButtonGroup aria-label="Text style" className="toggle-button-group">
-                                <ToggleButton id="bold" className="toggle-button">
+                            <ToggleButtonGroup aria-label="Text style" className="toggleGroup">
+                                <ToggleButton id="bold" className="toggle">
                                     Bold
                                 </ToggleButton>
-                                <ToggleButton id="italic" className="toggle-button">
+                                <ToggleButton id="italic" className="toggle">
                                     Italic
                                 </ToggleButton>
-                                <ToggleButton id="underline" className="toggle-button">
+                                <ToggleButton id="underline" className="toggle">
                                     Underline
                                 </ToggleButton>
                             </ToggleButtonGroup>
