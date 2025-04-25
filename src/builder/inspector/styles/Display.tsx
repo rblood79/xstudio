fed2a57edea9212f6d0d6f8e7ed3ebfe33aaa599
rblood79 <rblood79@gmail.com>
2025-04-26 00:45:57 +0900

    refactor: update alignment and padding in Display component, enhance layout styles in inspector and sidebar

--- src/builder/inspector/styles/Display.tsx
+++ src/builder/inspector/styles/Display.tsx
@@ -19,20 +19,34 @@ function Display() {
                 </div>
                 <div className='panel-content'>
                     <fieldset>
-                        <legend className='position-legend'>Alignment</legend>
-                        <div className='position-horizontal'>
+                        <legend className='alignment-legend'>Alignment</legend>
+                        <div className='alignment-horizontal'>
                             <ToggleButtonGroup>
                                 <ToggleButton>L</ToggleButton>
                                 <ToggleButton>M</ToggleButton>
                                 <ToggleButton>R</ToggleButton>
                             </ToggleButtonGroup>
                         </div>
-                        <div className='position-vertical'>
+                        <div className='alignment-vertical'>
                             <ToggleButtonGroup>
                                 <ToggleButton>T</ToggleButton>
                                 <ToggleButton>C</ToggleButton>
                                 <ToggleButton>B</ToggleButton>
                             </ToggleButtonGroup>
+                        </div>
+                        <div className='alignment-distribution'>
+                            <Button>:</Button>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <legend className='position-legend'>Position</legend>
+                        <div className='position-x'>
+                            <label>x</label>
+                            <input></input>
+                        </div>
+                        <div className='position-y'>
+                            <label>y</label>
+                            <input></input>
                         </div>
                         <div className='position-distribution'>
                             <Button>:</Button>
@@ -51,16 +65,8 @@ function Display() {
                         ><ChevronUp color={iconProps.color} strokeWidth={iconProps.stroke} size={iconProps.size} /></button>
                     </div>
                 </div>
-                <div>
-                    <fieldset>
-                        <legend className='legend'>Display Settings</legend>
-                    </fieldset>
-                    <div className="display-options">
-                        <label htmlFor="option1">Option 1</label>
-                        <input type="checkbox" id="option1" />
-                        <label htmlFor="option2">Option 2</label>
-                        <input type="checkbox" id="option2" />
-                    </div>
+                <div className='panel-content'>
+
                 </div>
             </div>
 
