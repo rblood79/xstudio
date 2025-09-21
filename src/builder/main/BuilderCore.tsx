7b08829deceb9a9df0034b4bb651d59b175b25f2
rblood79 <rblood79@gmail.com>
2025-09-21 17:21:50 +0900

    refactor: Remove BuilderFooter component and associated styles, replacing it with Monitor component in BuilderCore for enhanced functionality

--- src/builder/main/BuilderCore.tsx
+++ src/builder/main/BuilderCore.tsx
@@ -20,7 +20,7 @@ import { useIframeMessenger } from '../hooks/useIframeMessenger';
 import { useThemeManager } from '../hooks/useThemeManager';
 import { useValidation } from '../hooks/useValidation';
 import { memoryMonitor } from '../utils/memoryMonitor';
-import { BuilderFooter } from '../footer'; // BuilderFooter 컴포넌트 임포트
+import { Monitor } from '../monitor'; // BuilderFooter 컴포넌트 임포트
 
 import './index.css';
 import { MessageService } from '../../utils/messaging';
@@ -336,7 +336,7 @@ export const BuilderCore: React.FC = () => {
 
                 {/* 기존 footer 태그를 BuilderFooter 컴포넌트로 대체 */}
                 <footer className="footer">
-                    <BuilderFooter />
+                    <Monitor />
                 </footer>
             </BuilderViewport>
         </div>
